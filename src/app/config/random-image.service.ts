f7d65781ff040a64cdb3cf22dd6a858409ed87b9
Ahmed gamal <ahmed.gamal@musala.com>
2022-10-06 00:53:22 +0300

    refactor

--- src/app/config/random-image.service.ts
+++ src/app/config/random-image.service.ts
@@ -1,40 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Root } from './models/root';
-
-
 
 // Root
 
 @Injectable()
 export class RandomImageService {
-    accessKey = "pVsUuikCp69QhfcJ4V7jQKY-As6DICJjnux1tLzBeD8"
-    UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${this.accessKey}`;
+  accessKey = 'pVsUuikCp69QhfcJ4V7jQKY-As6DICJjnux1tLzBeD8';
+  UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${this.accessKey}`;
 
+  private image: string = '';
 
-    private image: string= ""; 
-
-    constructor(private http: HttpClient) {
-      this.getImageUrls().subscribe(
-        (results: Root) => {
-            console.log("results",results.color)
-            console.log("full",results.urls.full)
-
-            this.image = results.urls.full
-        }
-      )
-    }
-  
-     public getRandomImage(): string {
-      return this.image
-     }
-  
-     public getImageUrls(): Observable<Root> {
-      return this.http.get<any>(this.UNSPLASH_URL)
-        .pipe(
-          map((response: Root) => response)
-        );
-    }
+  constructor(private http: HttpClient) {
+    this.getImageUrls().subscribe((results: Root) => {
+      this.image = results.urls.full;
+    });
   }
+
+  public getRandomImage(): string {
+    return this.image;
+  }
+
+  public getImageUrls(): Observable<Root> {
+    return this.http
+      .get<any>(this.UNSPLASH_URL)
+      .pipe(map((response: Root) => response));
+  }
+}
